49a6a96b7e8d9cdc6a757650605b08ad389b6b56
Mohamed-magdy65 <medomagdy004@gmail.com>
2025-09-27 02:54:02 +0300

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout <<"Hi my name is Mohamed\n";
+    cout <<"I Will Start To Learning programming";
+    cout << "\n I Hope it is a good start and progress in the future";
+    cout <<"\n see you all";
+    return 0;
+}
